de25e2ab177d6d36008888e61e0aef7c361c63ab
vivien <vivien.korpys@manuloc.eu>
2024-12-05 21:35:35 +0100

    Add new resources and assets for the application
    Introduced new fonts, raw assets, images, and SVG icons to enhance the application's appearance and functionality. These additions are critical for proper rendering and user experience across various platforms. The raw assets' deployment is automated through the MauiAsset Build Action defined in the .csproj file.

---
+++ SDWorx.PayCheck/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace SDWorx.PayCheck;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "SDWorx.PayCheck" };
+    }
+}
---
+++ SDWorx.PayCheck/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SDWorx.PayCheck;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
