1cc12d4c08a8f45ad664f3809706cd961397b19f
vivien <vivien.korpys@manuloc.eu>
2024-12-29 00:00:58 +0100

    Add new app icon and update UI configurations
    Added a new SVG app icon and updated project to include it. Modified window dimensions to set fixed height and width while updating minimum constraints. Adjusted UI text for clarity and consistency.

--- SDWorx.PayCheck/App.xaml.cs
+++ SDWorx.PayCheck/App.xaml.cs
@@ -10,8 +10,12 @@ public partial class App : Application
     protected override Window CreateWindow(IActivationState? activationState)
     {
         var windows  = new Window(new MainPage()) { Title = "SDWorx.PayCheck" };
-        windows.MinimumWidth = 1000;
-        windows.MinimumHeight = 800;
+        windows.MinimumWidth = 1200;
+        windows.MinimumHeight = 700;
+        windows.Height = 700;
+        windows.Width = 1200;
+        windows.MaximumHeight = 700;
+        windows.MaximumWidth = 1200;
         return windows;
     }
 }
